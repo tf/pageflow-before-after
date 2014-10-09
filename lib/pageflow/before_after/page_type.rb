82b8ef5795c3c8844cae82977de3f2b585dea9a4
Tim Fischbach <tfischbach@codevise.de>
2014-10-09 20:03:18 +0200

    Define Thumbnail Candidates
    Starting with version 0.5, pageflow supports supplying custom
    thumbnails for new page types.

--- lib/pageflow/before_after/page_type.rb
+++ lib/pageflow/before_after/page_type.rb
@@ -2,6 +2,13 @@ module Pageflow
   module BeforeAfter
     class PageType < Pageflow::PageType
       name 'before_after'
+
+      def thumbnail_candidates
+        [
+          {attribute: 'thumbnail_image_id', file_collection: 'image_files'},
+          {attribute: 'after_image_id', file_collection: 'image_files'}
+        ]
+      end
     end
   end
 end
